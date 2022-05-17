7cf0a872dc1e788ad7a9f7116a2a6939e9278843
Gigi-Didberidze <gigididberidze01@gmail.com>
2022-05-17 21:49:41 +0200

    Corrected package in Main.java and added game implementation for GameApp.java

--- src/main/java/ColorCollection/GameApp.java
+++ src/main/java/ColorCollection/GameApp.java
@@ -1,2 +1,27 @@
-public class GameApp {
+package ColorCollection;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class GameApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        StackPane root = new StackPane();
+        root.setStyle("-fx-background-color: LAVENDER");
+        Text text = new Text("Color Collection Board Game");
+        text.setFont(Font.font("Verdana", FontWeight.BOLD, 50));
+        text.setFill(Color.BLACK);
+        root.getChildren().add(text);
+        Scene scene = new Scene(root, 900, 600);
+        stage.setTitle("Color Collection");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 }
